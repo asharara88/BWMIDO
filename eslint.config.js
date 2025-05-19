9a161b18e35da8fd23d54684f504790efa933f87
asharara88 <ahmed.m.sharara@gmail.com>
2025-05-19 17:02:26 +0400

    refactor: use dynamic imports in ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,57 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+export default async function () {
+  try {
+    const js = (await import('@eslint/js')).default;
+    const globals = (await import('globals')).default;
+    const reactHooks = (await import('eslint-plugin-react-hooks')).default;
+    const reactRefresh = (await import('eslint-plugin-react-refresh')).default;
+    const tseslint = (await import('typescript-eslint')).default;
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+    return tseslint.config(
+      { ignores: ['dist'] },
+      {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+          ecmaVersion: 2020,
+          globals: globals.browser,
+        },
+        plugins: {
+          'react-hooks': reactHooks,
+          'react-refresh': reactRefresh,
+        },
+        rules: {
+          ...reactHooks.configs.recommended.rules,
+          'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+          ],
+        },
+      }
+    );
+  } catch {
+    const globals = (await import('globals')).default;
+    const reactHooks = (await import('eslint-plugin-react-hooks')).default;
+    const reactRefresh = (await import('eslint-plugin-react-refresh')).default;
+
+    return [
+      {
+        ignores: ['dist'],
+        languageOptions: {
+          ecmaVersion: 2020,
+          globals: globals.browser,
+        },
+        plugins: {
+          'react-hooks': reactHooks,
+          'react-refresh': reactRefresh,
+        },
+        rules: {
+          ...reactHooks.configs.recommended.rules,
+          'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+          ],
+        },
+      },
+    ];
   }
-);
+}
