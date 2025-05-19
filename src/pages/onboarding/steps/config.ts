886d6e51b91b9869da2848d562763c6e799ce826
asharara88 <ahmed.m.sharara@gmail.com>
2025-05-19 16:54:03 +0400

    Add modular onboarding steps

---
+++ src/pages/onboarding/steps/config.ts
@@ -0,0 +1,15 @@
+import WelcomeStep from './WelcomeStep';
+import PersonalInfoStep from './PersonalInfoStep';
+import HealthGoalsStep from './HealthGoalsStep';
+import LifestyleStep from './LifestyleStep';
+import WearablesStep from './WearablesStep';
+import CompletionStep from './CompletionStep';
+
+export default [
+  WelcomeStep,
+  PersonalInfoStep,
+  HealthGoalsStep,
+  LifestyleStep,
+  WearablesStep,
+  CompletionStep,
+];
