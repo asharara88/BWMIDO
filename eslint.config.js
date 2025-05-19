86660a3e11e54af7310ca34b07aa2dca76fba83f
asharara88 <ahmed.m.sharara@gmail.com>
2025-05-19 17:01:14 +0400

    feat: handle missing ESLint deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,52 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+let js;
+let globals;
+let reactHooks;
+let reactRefresh;
+let tseslint;
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+let hasDeps = true;
+
+try {
+  js = await import('@eslint/js');
+  globals = await import('globals');
+  reactHooks = await import('eslint-plugin-react-hooks');
+  reactRefresh = await import('eslint-plugin-react-refresh');
+  tseslint = await import('typescript-eslint');
+} catch {
+  hasDeps = false;
+}
+
+let config;
+
+if (hasDeps && js && tseslint) {
+  config = tseslint.config(
+    { ignores: ['dist'] },
+    {
+      extends: [js.configs.recommended, ...tseslint.configs.recommended],
+      files: ['**/*.{ts,tsx}'],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+      plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+      },
+      rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': [
+          'warn',
+          { allowConstantExport: true },
+        ],
+      },
+    }
+  );
+} else {
+  config = [
+    {
+      ignores: ['dist', '**/*.ts', '**/*.tsx'],
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  }
-);
+  ];
+}
+
+export default config;
